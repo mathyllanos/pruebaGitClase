a5c5976b779f2345675418478604f1811c9425a0
Mateo Llanos Vallejos <mllaval116@dptoinformatica.iesfuengirola1.net>
2024-12-03 19:16:18 +0100

    Commit inicial del proyecto pruebaGitClase

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private float speed = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
